5fcb407840ab73ece381b0efddf1a1baa398932c
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-04-13 22:42:08 +0545

    Formatting

--- configs/passport.config.js
+++ configs/passport.config.js
@@ -12,9 +12,10 @@ function configPassport(app) {
   passport.use(
     new LocalStrategy(async (username, password, done) => {
       try {
-        const { rows } = await pool.query("SELECT * FROM users WHERE username=$1", [
-          username,
-        ]);
+        const { rows } = await pool.query(
+          "SELECT * FROM users WHERE username=$1",
+          [username]
+        );
         const user = rows[0];
         if (!user) {
           return done(null, false, { message: "User not available." });
@@ -36,7 +37,9 @@ function configPassport(app) {
 
   passport.deserializeUser(async (id, done) => {
     try {
-      const { rows } = await pool.query("SELECT * FROM users WHERE id=$1", [id]);
+      const { rows } = await pool.query("SELECT * FROM users WHERE id=$1", [
+        id,
+      ]);
       const user = rows[0];
       done(null, user);
     } catch (err) {
