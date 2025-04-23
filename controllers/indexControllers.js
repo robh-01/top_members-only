bec3e1c681c81c6ba0c30f234c5c5ad993b95055
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-04-23 22:26:20 +0545

    Add log out functionality

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -39,3 +39,12 @@ export const logInPost = [
     failureRedirect: "/failure",
   }),
 ];
+
+export function logOutGet(req, res, next) {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+}
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,5 +7,6 @@ indexRouter.get("/", indexController.indexPageGet);
 indexRouter.get("/sign-in", indexController.signInGet);
 indexRouter.post("/sign-in", indexController.signInPost);
 indexRouter.post("/log-in", indexController.logInPost);
+indexRouter.get("/log-out", indexController.logOutGet);
 
 export { indexRouter };
